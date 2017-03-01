1ee94ee02af95f3055ab00e6ee28a64b23474183
Stephanie Andrews <seandrews11@gmail.com>
2017-02-28 22:01:52 -0700

    completed a while ago but the video seems to be out of date- the code does not work and has since been updated on their site

--- side-nav/side-nav.js
+++ side-nav/side-nav.js
@@ -16,3 +16,87 @@
  */
 
 'use strict';
+
+class SideNav {
+  constructor() {
+    this.showButtonEl = document.querySelector('.js-menu-show');
+    this.hideButtonEl = document.querySelector('.js-menu-hide');
+    this.sideNavEl = document.querySelector('.js-side-nav');
+    this.sideNavContainerEl = document.querySelector('.js-side-nav-container');
+
+    this.showSideNav = this.showSideNav.bind(this);
+    this.hideSideNav = this.hideSideNav.bind(this);
+    this.blockClicks = this.blockClicks.bind(this);
+    this.onTouchStart = this.onTouchStart.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.onTouchEnd = this.onTouchEnd.bind(this);
+    this.onTransitionEnd = this.onTransitionEnd.bind(this);
+
+    this.startX = 0;
+    this.currentX = 0;
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.showButtonEl.addEventListener('click', this.showSideNav);
+    this.hideButtonEl.addEventListener('click', this.hideSideNav);
+    this.sideNavEl.addEventListener('click', this.hideSideNav)
+    this.sideNavContainerEl.addEventListener('click', this.blockClicks)
+
+    document.addEventListener('touchstart', this.onTouchStart)
+    document.addEventListener('touchmove', this.onTouchMove)
+    document.addEventListener('touchend', this.onTouchEnd)
+  }
+
+  onTouchStart (e) {
+    if (!this.sideNavEl.classList.contains('side-nav--visible'))
+    return
+
+  //  e.preventDefault();
+    this.startX = e.touches[0].pageX;
+    this.currentX = this.startX;
+  }
+
+  onTouchMove (e) {
+    this.currentX = e.touches[0].pageX;
+    const translateX = Math.min(0, this.currentX - this.startX)
+
+    if (this.translateX < 0) {
+      e.preventDefault();
+    }
+
+    this.sideNavContainerEl.style.transform = `translateX${translateX}px`
+  }
+
+  onTouchEnd (e) {
+    const translateX = Math.min(0, this.currentX - this.startX)
+    if (translateX < 0) {
+      this.sideNavContainerEl.style.transform = '';
+      this.hideSideNav();
+    }
+  }
+
+  blockClicks(e) {
+    e.stopPropagation()
+  }
+
+  onTransitionEnd (e) {
+    this.sideNavEl.classList.remove('.side-nav--animatable');
+    this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd);
+  }
+
+  showSideNav() {
+    this.sideNavEl.classList.add('.side-nav--animatable')
+    this.sideNavEl.classList.add('.side-nav--visible');
+    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd)
+  }
+
+  hideSideNav() {
+    this.sideNavEl.classList.add('.side-nav--animatable')
+    this.sideNaveEl.classList.remove('.side-nav--visible');
+    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
+  }
+}
+
+new SideNav();
